673138dea7090195b704934f321623b45d2ea4c3
preranadave <46474491+preranadave@users.noreply.github.com>
2019-03-29 23:48:01 +0530

    Add files via upload

---
+++ Practical-4/Practical3/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Customer
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public Customer(int ID, string Name)
+    {
+        this.ID = ID;
+        this.Name = Name;
+    }
+    public Customer()
+    {
+        this.ID = -1;
+        this.Name = string.Empty;
+    }
+    public void printID()
+    {
+        Console.WriteLine("ID is: {0}", this.ID);
+    }
+    public void printName()
+    {
+        Console.WriteLine("Name is: {0}", this.Name);
+    }
+}
---
+++ Practical-4/Practical3/Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Student
+{
+   public  class StudentOverload
+    {
+        StudentOverload(String Name)
+        {
+            Console.WriteLine(Name);
+        }
+        StudentOverload(String Name, long Enroll)
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Enroll);
+        }
+        StudentOverload(String Name, long Enroll, String Branch)
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Enroll);
+            Console.WriteLine(Branch);
+        }
+        public static void Main()
+        {
+            StudentOverload s1 = new StudentOverload("prerana");
+            StudentOverload s2 = new StudentOverload("prerana",170473107003);
+            StudentOverload s3 = new StudentOverload("prerana", 170473107003,"CE");
+        }
+
+    }
+}
