b89b7a24d97051512f11cea7a17920c70dead2ca
preranadave <46474491+preranadave@users.noreply.github.com>
2019-03-30 00:18:29 +0530

    Add files via upload

---
+++ Practical-6/StudentData/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace StudentData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.tctMname = new System.Windows.Forms.TextBox();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.rdoFemale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMobile = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateDob = new System.Windows.Forms.DateTimePicker();
+            this.imgStudent = new System.Windows.Forms.PictureBox();
+            this.btnImage = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(183, 49);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(262, 46);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(100, 20);
+            this.txtFname.TabIndex = 1;
+      
+            // 
+            // tctMname
+            // 
+            this.tctMname.Location = new System.Drawing.Point(385, 49);
+            this.tctMname.Name = "tctMname";
+            this.tctMname.Size = new System.Drawing.Size(100, 20);
+            this.tctMname.TabIndex = 2;
+            
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(511, 49);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(100, 20);
+            this.txtLname.TabIndex = 3;
+            
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(262, 99);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            
+            // 
+            // rdoFemale
+            // 
+            this.rdoFemale.AutoSize = true;
+            this.rdoFemale.Location = new System.Drawing.Point(385, 99);
+            this.rdoFemale.Name = "rdoFemale";
+            this.rdoFemale.Size = new System.Drawing.Size(59, 17);
+            this.rdoFemale.TabIndex = 5;
+            this.rdoFemale.TabStop = true;
+            this.rdoFemale.Text = "Female";
+            this.rdoFemale.UseVisualStyleBackColor = true;
+            
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Gender";
+           
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnImage);
+            this.groupBox1.Controls.Add(this.imgStudent);
+            this.groupBox1.Controls.Add(this.dateDob);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtMobile);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(161, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(692, 258);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Form";
+         
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mobile";
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Email";
+           
+            // 
+            // txtMobile
+            // 
+            this.txtMobile.Location = new System.Drawing.Point(101, 120);
+            this.txtMobile.Name = "txtMobile";
+            this.txtMobile.Size = new System.Drawing.Size(100, 20);
+            this.txtMobile.TabIndex = 2;
+         
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(281, 120);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 3;
+            
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "DOB";
+            
+            // 
+            // dateDob
+            // 
+            this.dateDob.Location = new System.Drawing.Point(101, 167);
+            this.dateDob.Name = "dateDob";
+            this.dateDob.Size = new System.Drawing.Size(200, 20);
+            this.dateDob.TabIndex = 8;
+            // 
+            // imgStudent
+            // 
+            this.imgStudent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgStudent.Location = new System.Drawing.Point(514, 23);
+            this.imgStudent.Name = "imgStudent";
+            this.imgStudent.Size = new System.Drawing.Size(95, 113);
+            this.imgStudent.TabIndex = 8;
+            this.imgStudent.TabStop = false;
+            // 
+            // btnImage
+            // 
+            this.btnImage.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnImage.Location = new System.Drawing.Point(514, 142);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(95, 23);
+            this.btnImage.TabIndex = 8;
+            this.btnImage.Text = "Upload Photo";
+            this.btnImage.UseVisualStyleBackColor = true;
+            this.btnImage.Click += new System.EventHandler(this.btnImage_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(375, 218);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(471, 218);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 484);
+            this.Controls.Add(this.rdoFemale);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.tctMname);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.TextBox tctMname;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rdoFemale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMobile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateDob;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnImage;
+        private System.Windows.Forms.PictureBox imgStudent;
+    }
+}
+
---
+++ Practical-6/StudentData/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string source = @"Data Source=ce3comp10\sqlexpress;Initial Catalog=Student;Integrated Security=True;Pooling=False";
+            string select = "select count(*) from tblStudent";
+            SqlConnection conn = new SqlConnection(source);
+            SqlCommand cmd = new SqlCommand(select, conn);
+            conn.Open();
+            int i = Convert.ToInt16(cmd.ExecuteScalar());
+            int pkStudent = i + 1;
+
+            string insert = "insert into Student_Data (pkStudent, fName,dob, imgStudent) values ( " + pkStudent + ",'" + txtFname.Text + "','" + dateDob.Value.Date + "','" + (imgPath == null ? "" : imgPath) + "' )";
+            cmd = new SqlCommand(insert, conn);
+
+            i = cmd.ExecuteNonQuery();
+            if (imgPath != null)
+                imgStudent.Image.Save(imgPath);
+            MessageBox.Show("You are Done!!!");
+            InitializeComponent();
+        }
+
+        private void btnImage_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Jpg|*.jpg";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imgPath = @"D:\DOT NET\Images\" + openFileDialog1.SafeFileName;
+                imgStudent.Image = Image.FromFile(openFileDialog1.FileName);
+                MessageBox.Show(imgPath);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
